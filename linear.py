fb44f650db7c77e7732c887cac2a2dbabd792fdd
ksatola <krzysztof@satola.net>
2019-06-09 11:38:37 +0200

    Update

---
+++ linear.py
@@ -0,0 +1,17 @@
+from typing import List
+from collections import Counter
+
+Vector = List[float]
+
+def dot(v: Vector, w: Vector) -> float:
+    """Computes v_1 * w_1 + ... + v_n * w_n"""
+    assert len(v) == len(w), "vectors must be same length"
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+assert dot([1, 2, 3], [4, 5, 6]) == 32  # 1 * 4 + 2 * 5 + 3 * 6
+
+def sum_of_squares(v: Vector) -> float:
+    """Returns v_1 * v_1 + ... + v_n * v_n"""
+    return dot(v, v)
+
+assert sum_of_squares([1, 2, 3]) == 14  # 1 * 1 + 2 * 2 + 3 * 3
